1664cfb6f94130e0287e751fd5354fb38d08ab12
Dimex19 <72265654+Dimex19@users.noreply.github.com>
2022-06-22 01:09:03 +0100

    Update 100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,71 +1,76 @@
 #!/usr/bin/python3
-"""Square related feature module"""
+"""this is a module docstring?"""
 
 
 class Node:
-    """ A class that defines a square by its size
-    """
+    """define a node"""
+
     def __init__(self, data, next_node=None):
-    """Method that initialize next node"""
+        """this is the init"""
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-    """Method that returns self"""
+        """gets data"""
         return self.__data
 
     @data.setter
     def data(self, value):
-    """Method that raises TypeError"""
+        """sets data"""
         if not isinstance(value, int):
-            raise TypeError("data must be an integer")
+            raise TypeError('data must be an integer')
         self.__data = value
 
     @property
     def next_node(self):
-    """Method that moves to next node"""
+        """get the next node for this node
+        Returns:
+            the next node
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-    """Method that checks next node"""
-        if not isinstance(value, Node) and value is not None:
-            raise TypeError("next_node must be a Node object")
+        """sets node"""
+        if value is not None and type(value) != Node:
+            raise TypeError('next_node must be a Node object')
         self.__next_node = value
 
 
 class SinglyLinkedList:
-"""Class of the code"""
-    def __str__(self):
-    """Method that prints"""
-        rtn = ""
-        ptr = self.__head
-
-        while ptr is not None:
-            rtn += str(ptr.data)
-            if ptr.next_node is not None:
-                rtn += "\n"
-            ptr = ptr.next_node
-
-        return rtn
+    """define a sll"""
 
     def __init__(self):
-    """Method that initialize"""
-        self.__head = None
+        """this is init"""
+        self.head = None
+
+    def __str__(self):
+        """teach python to print my way
+        Returns: the printed thing"""
+        printsll = ""
+        location = self.head
+        while location:
+            printsll += str(location.data) + "\n"
+            location = location.next_node
+        return printsll[:-1]
 
     def sorted_insert(self, value):
-    """Method that returns a value"""
-        ptr = self.__head
-
-        while ptr is not None:
-            if ptr.data > value:
-                break
-            ptr_prev = ptr
-            ptr = ptr.next_node
-
-        newNode = Node(value, ptr)
-        if ptr == self.__head:
-            self.__head = newNode
-        else:
-            ptr_prev.next_node = newNode
+        """insert in a sorted fashion
+        Args:
+            value: what the value will be on the node
+        """
+        new = Node(value)
+        if not self.head:
+            self.head = new
+            return
+        if value < self.head.data:
+            new.next_node = self.head
+            self.head = new
+            return
+        location = self.head
+        while location.next_node and location.next_node.data < value:
+            location = location.next_node
+        if location.next_node:
+            new.next_node = location.next_node
+        location.next_node = new
